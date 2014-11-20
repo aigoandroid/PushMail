c98ca7089bef19cdb53eb78687347b08cd3ad3cd
phuocth <hoangphuoc18@yahoo.com>
2014-11-20 17:59:02 +0700

    Implement GCMcomponent to main activity

--- Source/src/com/aigovn/pushmail/MainActivity.java
+++ Source/src/com/aigovn/pushmail/MainActivity.java
@@ -4,39 +4,52 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class MainActivity extends Activity implements INavigation {
-	
+import com.aigovn.google.gcm.GCMManager;
+import com.aigovn.google.gcm.IGCMListener;
+
+public class MainActivity extends Activity implements INavigation, IGCMListener {
+
 	FragmentManager fMan;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		
+
 		setContentView(R.layout.main_activity);
-		
+
+		// register GCM
+		GCMManager gcmMan = GCMManager.getInstance(this, this);
+		if (gcmMan != null) {
+			if (gcmMan.getRegistrationId().isEmpty()) {
+				gcmMan.registerGCM(Constant.GCM_SENDER_ID);
+			}
+		}
+
 		fMan = getFragmentManager();
 		InboxFragment fragment = new InboxFragment();
 		setHomeContent(fragment);
 	}
-	
+
 	@Override
 	public void onBackPressed() {
-		if(fMan.getBackStackEntryCount() == 1){
+		if (fMan.getBackStackEntryCount() == 1) {
 			finish();
-		}else{
+		}
+		else {
 			super.onBackPressed();
 		}
 	}
 
 	@Override
-	public void setHomeContent(Fragment fragment){
+	public void setHomeContent(Fragment fragment) {
 		String fragName = fragment.getClass().getName();
 		FragmentTransaction fTrans = fMan.beginTransaction();
 		fTrans.add(R.id.home_content, fragment, fragName);
@@ -48,7 +61,7 @@ public class MainActivity extends Activity implements INavigation {
 	@Override
 	public void showNavigationMenu() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -66,6 +79,23 @@ public class MainActivity extends Activity implements INavigation {
 		FragmentManager fMan = getFragmentManager();
 		fMan.popBackStack(fragName, 0);
 	}
-	
-	
+
+	@Override
+	public void onGCMRegistrationSuccess(String deviceToken) {
+		// TODO Send device token to Server
+
+	}
+
+	@Override
+	public void onGCMRegistrationFailed(String message) {
+		// TODO show error message
+
+	}
+
+	@Override
+	public void onGCMNotificationReceived(Intent intent) {
+		// TODO handle pushed message
+
+	}
+
 }
